322c2220e9eb2d480c7b34b75c80650c8e4740d1
nsy <k.n1sh1y4@gmail.com>
2024-02-07 02:28:01 +0900

    add grpc reflection&auth

--- grpc-go/api/server.go
+++ grpc-go/api/server.go
@@ -7,7 +7,13 @@ import (
 	"net"
 
 	"github.com/4x2Hach1/learning/grpc-go/api/pb"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type service struct {
@@ -18,14 +24,30 @@ func (srv *service) Hello(ctx context.Context, p *pb.HelloRequest) (*pb.HelloRes
 	return &pb.HelloResponse{Message: "Hello"}, nil
 }
 
+func auth(ctx context.Context) (context.Context, error) {
+	token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
+	if err != nil {
+		return nil, err
+	}
+
+	if token != "auth" {
+		return nil, status.Error(codes.Unauthenticated, "token is invalid")
+	}
+
+	return ctx, nil
+}
+
 func main() {
 	lis, err := net.Listen("tcp", "localhost:5555")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+		grpc_auth.UnaryServerInterceptor(auth),
+	)))
 	pb.RegisterServerServiceServer(s, &service{})
+	reflection.Register(s)
 
 	fmt.Println("server is running...")
 	if err := s.Serve(lis); err != nil {
